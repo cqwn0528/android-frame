c08c7ce76b2fb05c095d90b76385acacb7983c4c
ciqingweini@163.com <ciqingweini0528>
2020-12-21 17:35:57 +0800

    1- 添加 mvp

--- app/src/main/java/com/android/frame_master/MainActivity.kt
+++ app/src/main/java/com/android/frame_master/MainActivity.kt
@@ -2,9 +2,9 @@ package com.android.frame_master
 
 import android.os.Bundle
 import android.util.Log
-import com.android.frame_master.data.`fun`.TimerFun
 import com.android.frame_master.base.BasicActivity
 import com.android.frame_master.bean.UserBean
+import com.android.frame_master.data.`fun`.TimerFun
 import com.android.frame_master.data.net.HttpUtil
 import com.android.frame_master.data.net.http.HttpCallback
 import com.android.frame_master.util.loadPic
@@ -14,7 +14,7 @@ class MainActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         loadPic(
             "https://pics6.baidu.com/feed/03087bf40ad162d9bb58337aabe817eb8b13cde5.png?token=802921089c14142f5e1f40864f25ac59&s=90B75D904A0372C0059F915D030090A2",
             img1
@@ -23,11 +23,11 @@ class MainActivity : BasicActivity() {
         btn1.setOnClickListener {
             TimerFun.startTiming(object : TimerFun.TimerInterface {
                 override fun timing(time: Int) {
-                    Log.e("几十种", "" + time)
+                    Log.e("开始倒计时", "" + time)
                 }
 
                 override fun timeEnds() {
-                    Log.e("几十结束", "哈哈")
+                    Log.e("结束倒计时", "哈哈")
                 }
             })
         }
--- app/src/main/java/com/android/frame_master/Text.kt
+++ app/src/main/java/com/android/frame_master/Text.kt
@@ -2,10 +2,8 @@ package com.android.frame_master
 
 internal class Text {
     var name = ""
-
     constructor(id: String) {
         name = id
     }
-
     constructor(id: String?, type: Int) {}
 }
